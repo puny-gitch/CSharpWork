2d933ce16c3816ef11bded3c5c9c942b33f65379
puny-gitch <3179857925@qq.com>
2025-02-25 11:03:33 +0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("你好！我是黎宇恒");
+        }
+    }
+}
